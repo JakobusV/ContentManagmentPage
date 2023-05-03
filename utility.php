b88d7ebce79d49bce74c823162df5a69ce317e3e
JakobusV <51762364+JakobusV@users.noreply.github.com>
2023-05-03 10:45:40 -0600

    Enter personal conneciton data

--- utility.php
+++ utility.php
@@ -2,4 +2,18 @@
 function IsNullOrEmptyString($str){
     return ($str === null || trim($str) === '');
 }
+
+function DBConnection() {
+    $host = "";
+    $port = 0;
+    $socket = "";
+    $user = "";
+    $password = "";
+    $dbname = "";
+
+    $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+        or die ('Could not connect to the database server' . mysqli_connect_error());
+
+    return $con
+}
 ?>

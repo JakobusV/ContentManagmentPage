39c77915aedd66566dfbc059e177b4591eed1115
JustinW1233 <79864211+JustinW1233@users.noreply.github.com>
2023-05-10 09:34:07 -0600

    Login page creation

---
+++ Login.php
@@ -0,0 +1,25 @@
+<?php
+include_once "header.php";
+include_once "loginCSS";
+?>
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>HTML, CSS and JavaScript demo</title>
+        <link rel="stylesheet" href="loginCSS.php"/>
+    </head>
+
+    <body class="background">
+        <div class="loginBlock">
+            <h1>Login</h1>
+            <p class="loginBlockItem">Email</p>
+            <input class="loginBlockItem" type="text" placeholder="✉️Type your email here" />
+            <p class="loginBlockItem">Password</p>
+            <input class="loginBlockItem passwordBottom" type="text" placeholder="🔒Type your password here" />
+            <input type="submit" class="loginSubmit" />
+            <href></href>
+        </div>
+    </body>
+
+</html>
---
+++ loginCSS.php
@@ -0,0 +1,58 @@
+<?php
+header("Content-type: text/css");
+?>
+
+.loginBlock {
+    border: 3px solid whitesmoke;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    height: fit-content;
+}
+
+body {
+    background-image: url(https://c4.wallpaperflare.com/wallpaper/46/800/989/skull-fire-flame-dark-wallpaper-preview.jpg);
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+}
+
+h1 {
+    align-self: center;
+    color: #F5F5F5;
+    font: 50px Arial, sans-serif;
+    margin: 10px;
+    margin-bottom: 0px;
+}
+
+p {
+    font: 18px Arial, sans-serif;
+    color: #F5F5F5;
+}
+
+.loginSubmit {
+    align-self: center;
+    border-radius: 20px;
+    border: 0px;
+    color: #F5F5F5;
+    font-size: 20px;
+    margin: 10px;
+    width: 200px;
+    background: rgb(236, 255, 47);
+    background: linear-gradient(0deg, rgba(236, 255, 47, 1) 0%, rgba(255, 141, 0, 1) 50%, rgba(255, 0, 0, 1) 100%);
+}
+
+.loginBlockItem {
+    align-self: center;
+    border-left: 0;
+    border-right: 0;
+    border-top: 0;
+    margin-bottom: 10px;
+    width: 200px;
+}
+
+input {
+    background: #FFFFFF00;
+}

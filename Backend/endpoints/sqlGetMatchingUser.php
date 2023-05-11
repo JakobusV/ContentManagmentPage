bbadb9de4f6c705565fa19dc37f1a5c442a45768
Joseph Baker <joeyhbaker@gmail.com>
2023-05-11 12:03:25 -0600

    Get Matching User with Email and Password

--- Backend/endpoints/sqlGetMatchingUser.php
+++ Backend/endpoints/sqlGetMatchingUser.php
@@ -13,7 +13,21 @@ if (array_key_exists("userEmail", $data) && array_key_exists("userPassword", $da
     $userPassword = $data["userPassword"];
     $user = new user();
 
+    $query = $user->SelectQuery($columns = array(), $filters = array($user->CreateFilterExact("email", $userEmail), $user->CreateFilterExact("password", $userPassword)));
+    $dataSet = @mysqli_query($dbConnection, $query);
 
+    if ($dataSet) {
+        $rowArray;
+        while ($row = @mysqli_fetch_array($dataSet)) {
+            $rowArray[] = json_decode($row[0]);
+        }
+        $json = json_encode($rowArray);
+    }
+    @mysqli_close($dbConnection);
+
+    if ($json == "null") {
+        $json = "No Matching Users";
+    }
 }
 echo $json;
 ?>

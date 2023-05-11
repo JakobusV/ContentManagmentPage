b616e5c28755b21780704e0808640b02e086c151
ColeBelfry <cbelfry@student.neumont.edu>
2023-05-11 11:07:37 -0600

    session and cookie functions

--- utility.php
+++ utility.php
@@ -11,4 +11,49 @@ function DBConnection() {
 
     return $con;
 }
+/*
+Creates a new cookie
+use for html style preferences
+*/
+function CreateCookie($name, $value, $lifetime){
+    if(!isset($_COOKIE[$name])){
+        setcookie($name, $value, time()+$lifetime);
+        return true;
+    } else {
+        return false;
+    }
+}
+/*
+Kills the given cookie
+*/
+function KillCookie($name){
+    if(isset($_COOKIE[$name])){
+        setcookie($name, "", time()-10000);
+        return true;
+    } else {
+        return false;
+    }
+}
+/** 
+ * Creates a new session
+*/
+//Use for account details
+function CreateSession($name, $value){
+    if(!isset($_SESSION[$name])){
+        $_SESSION[$name] = $value;
+        return true;
+    } else {
+        return false;
+    }
+}
+/**
+ * Destroys the given session */
+function DestroySession($name){
+    if(isset($_SESSION[$name])){
+        unset($_SESSION[$name]);
+        return true;
+    } else {
+        return false;
+    }
+}
 ?>

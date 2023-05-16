5a5ee6204959ade0ab01aab425617450a5f585b5
JakobusV <51762364+JakobusV@users.noreply.github.com>
2023-05-16 17:01:19 -0600

    echo header for admin page

--- AdminAddUpdate.php
+++ AdminAddUpdate.php
@@ -2,12 +2,8 @@
 include_once 'header.php';
 
 
-if (!isset($_SESSION["current_user"]["auth"]))
-    header("Location: Login.php");
-else if ($_SESSION["current_user"]["auth"] != 1)
-    header("Location: contentPage.php");
 
-GenerateHeader("Admin Add/Update");
+echo GenerateHeader("Admin Add/Update");
 ?>
 <section>
     <div class="cat">
